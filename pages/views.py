1af37f6a27be108fef5101c673024858053571e6
mavg2110 <mavergara2@eafit.edu.co>
2026-02-22 18:48:52 -0500

    refactor(views): deleted duplicated imports

--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.http import HttpResponseRedirect
-from django.views.generic import TemplateView
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic import TemplateView, ListView
 from django.views import View
 from django import forms
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from .models import Product
-from django.shortcuts import get_object_or_404
 from django.core.exceptions import ValidationError
 
 # Create your views here.
@@ -48,8 +45,8 @@ class ProductShowView(View):
         try:
             product_id = int(id)
             if product_id < 1:
-                raise ValueError()
-        except ValueError:
+                raise ValueError("Product id must be 1 or greater")
+        except (ValueError, IndexError):
             return HttpResponseRedirect(reverse('home'))
 
         product = get_object_or_404(Product, pk=product_id)
@@ -95,6 +92,17 @@ class ProductCreateView(View):
             viewData["title"] = "Create product"
             viewData["form"] = form
             return render(request, self.template_name, viewData)
+
+class ProductListView(ListView): 
+    model = Product 
+    template_name = 'product_list.html' 
+    context_object_name = 'products'  # This will allow you to loop through 'products' in your template 
+
+    def get_context_data(self, **kwargs): 
+        context = super().get_context_data(**kwargs) 
+        context['title'] = 'Products - Online Store' 
+        context['subtitle'] = 'List of products' 
+        return context 
 
 class ContactPageView(TemplateView):
     template_name = 'pages/contact.html'
