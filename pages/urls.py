ad12171e05fe42b2418a21017c242dc20ffb2132
mavg2110 <mavergara2@eafit.edu.co>
2026-03-01 18:12:19 -0500

    feat(images): implement image upload with DI and non-DI versions

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, ProductIndexView, ProductShowView, ProductCreateView, ContactPageView, CartView, CartRemoveAllView
+from .views import HomePageView, AboutPageView, ProductIndexView, ProductShowView, ProductCreateView, ContactPageView, CartView, CartRemoveAllView, ImageViewFactory, ImageLocalStorage, ImageViewNoDI
 
 
 urlpatterns = [
@@ -12,4 +12,8 @@ urlpatterns = [
     path('cart/', CartView.as_view(), name='cart_index'),
     path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
     path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
+    path('image/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_index'),
+    path('image/save', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_save'),
+    path('imagenotdi/', ImageViewNoDI.as_view(), name='imagenodi_index'), 
+    path('imagenotdi/save', ImageViewNoDI.as_view(), name='imagenodi_save'), 
 ]
--- pages/views.py
+++ pages/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from .models import Product
 from django.core.exceptions import ValidationError
+from .utils import ImageLocalStorage
 
 # Create your views here.
 
@@ -151,3 +152,36 @@ class CartRemoveAllView(View):
             del request.session['cart_product_data']
 
         return redirect('cart_index')
+
+def ImageViewFactory(image_storage):
+
+    class ImageView(View):
+
+        template_name = 'images/index.html'
+
+        def get(self, request):
+            image_url = request.session.get('image_url', '')
+            return render(request, self.template_name, {'image_url': image_url})
+
+        def post(self, request):
+            image_url = image_storage.store(request)
+            request.session['image_url'] = image_url
+            return redirect('image_index')
+
+    return ImageView
+
+class ImageViewNoDI(View): 
+
+    template_name = 'images/index.html' 
+
+    def get(self, request): 
+        image_url = request.session.get('image_url', '') 
+        
+        return render(request, self.template_name, {'image_url': image_url}) 
+    
+    def post(self, request): 
+        image_storage = ImageLocalStorage() 
+        image_url = image_storage.store(request) 
+        request.session['image_url'] = image_url 
+        
+        return redirect('image_index')
