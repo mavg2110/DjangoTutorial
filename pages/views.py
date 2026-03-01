58d271d87a3d1648ee5a8440ec007fbdf4eb1a3f
mavg2110 <mavergara2@eafit.edu.co>
2026-03-01 16:23:57 -0500

    feat(cart): add cart classes, template and routes

--- pages/views.py
+++ pages/views.py
@@ -106,3 +106,48 @@ class ProductListView(ListView):
 
 class ContactPageView(TemplateView):
     template_name = 'pages/contact.html'
+
+class CartView(View):
+
+    template_name = 'cart/index.html'
+
+    def get(self, request):
+
+        # Simulated database for products
+        products = {}
+        products[121] = {'name': 'Tv samsung', 'price': '1000'}
+        products[11] = {'name': 'Iphone', 'price': '2000'}
+
+        # Get cart products from session
+        cart_products = {}
+        cart_product_data = request.session.get('cart_product_data', {})
+
+        for key, product in products.items():
+            if str(key) in cart_product_data.keys():
+                cart_products[key] = product
+
+        view_data = {
+            'title': 'Cart - Online Store',
+            'subtitle': 'Shopping Cart',
+            'products': products,
+            'cart_products': cart_products
+        }
+
+        return render(request, self.template_name, view_data)
+
+    def post(self, request, product_id):
+
+        cart_product_data = request.session.get('cart_product_data', {})
+        cart_product_data[product_id] = product_id
+        request.session['cart_product_data'] = cart_product_data
+
+        return redirect('cart_index')
+
+class CartRemoveAllView(View):
+
+    def post(self, request):
+
+        if 'cart_product_data' in request.session:
+            del request.session['cart_product_data']
+
+        return redirect('cart_index')
